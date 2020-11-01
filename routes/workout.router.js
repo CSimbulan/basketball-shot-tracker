4223f7538784066823b73dfb8eefe0e5ad76b3b2
csimbulan <c.simbulan@hotmail.com>
2020-11-01 13:27:49 -0500

    added doc

--- routes/workout.router.js
+++ routes/workout.router.js
@@ -4,8 +4,14 @@ Api routes for workouts.
 
 const router = require("express").Router();
 
+/*
+Load controllers
+*/
 const { getWorkout, getWorkoutById, deleteWorkoutById, createWorkout } = require("../controllers/workout.controller.js");
 
+/*
+Assign controller to routes
+*/
 router.route("/").get(getWorkout);
 router.route("/:id").get(getWorkoutById);
 router.route("/:id").delete(deleteWorkoutById);
