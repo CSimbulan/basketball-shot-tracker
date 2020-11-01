ea0d842fa6aa73cb1e37ba48438d86c08986fe09
csimbulan <c.simbulan@hotmail.com>
2020-11-01 13:19:52 -0500

    added workout controller

---
+++ controllers/workout.controller.js
@@ -0,0 +1,51 @@
+/*
+Api controller for workout.
+*/
+
+let Workout = require("../models/workout.model");
+
+/*
+Normal get request, return all workouts.
+*/
+exports.getWorkout = (req, res) => {
+    Workout.find()
+        .then((workout) => res.json(workout))
+        .catch((err) => res.status(400).json("Error: " + err));
+}
+
+/*
+Get request for a specific workout given an ID.
+*/
+exports.getWorkoutById = (req, res) => {
+    Workout.findById(req.params.id)
+        .then((workout) => res.json(workout))
+        .catch((err) => res.status(400).json("Error: " + err));
+}
+
+/*
+Delete a specific workout given an ID.
+*/
+exports.deleteWorkoutById = (req, res) => {
+    Workout.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Workout deleted."))
+        .catch((err) => res.status(400).json("Error: " + err));
+}
+
+/*
+Post request for creating a new workout.
+*/
+exports.createWorkout = (req, res) => {
+
+    workoutFields = {};
+    workoutFields.shotList = req.body.shotList;
+    workoutFields.startdate = Date.parse(req.body.startdate);
+    if (req.body.description) { workoutFields.description = req.body.description; }
+    if (req.body.enddate) { workoutFields.enddate = Date.parse(req.body.enddate); }
+
+    const newWorkout = new Workout(workoutFields);
+
+    newWorkout
+        .save()
+        .then(() => res.json("workout added!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+}
