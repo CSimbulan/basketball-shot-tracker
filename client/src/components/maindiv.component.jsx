a78bb3b570845f29316747ea1aee0e9a9531db8c
csimbulan <c.simbulan@hotmail.com>
2020-11-07 02:53:49 -0500

    moved shot listing to separate component

--- client/src/components/maindiv.component.jsx
+++ client/src/components/maindiv.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBBtnGroup, MDBTooltip } from "mdbreact";
+import { MDBRow, MDBCol, MDBBtn } from "mdbreact";
 import CourtCell from "./courtcell.component";
+import ShotListing from "./shotlisting.component"
 
 class Main extends Component {
     constructor(props) {
@@ -129,68 +130,17 @@ class Main extends Component {
         this.setState({ shotList: newSL });
     }
 
-    getShotPercentClass = (percentage) => {
-
-        if (percentage >= 100) {
-            return 'royalblue';
-        }
-        else if (percentage >= 80) {
-            return '#4287f5';
-        }
-        else if (percentage >= 50) {
-            return 'green';
-        }
-        else if (percentage >= 33) {
-            return '#fc7b03';
-        }
-        else if (percentage < 80) {
-            return 'red';
-        }
-        else {
-            return 'black';
-        }
-    }
-
     getShots = () => {
         return this.state.shotList.map((shot) => {
             return (
-                <div className="shotlist-item" key={"x" + shot.x + ",y" + shot.y}>
-                    <MDBContainer>
-                        <MDBRow size="12" style={{ width: "100%" }}>
-                            <MDBCol style={{ justifyContent: "center", alignItems: "center", margin: "auto" }}>
-                                <span className="court-mark" style={{ color: shot.markercolor }}>{shot.marker}</span>
-                            </MDBCol>
-                            <MDBCol size="9" lg="9">
-                                <MDBRow style={{ padding: "5px", alignItems: "center" }}>
-                                    x: {shot.x}, y: {shot.y}
-                                    <MDBBtnGroup size="sm" >
-                                        <MDBBtn color="danger" onClick={() => this.deleteShot(shot)}><i className="fas fa-trash-alt"></i></MDBBtn>
-                                    </MDBBtnGroup>
-                                </MDBRow>
-                                <MDBRow style={{ padding: "5px" }}>
-                                    FGM: {shot.makes} FGA: {shot.attemps} FG%: <span style={{ fontWeight: 500, color: this.getShotPercentClass(shot.makes / shot.attemps * 100) }}> {shot.attemps > 0 ? (shot.makes / shot.attemps * 100).toFixed(1) + "%" : "0.0%"}</span>
-                                </MDBRow>
-                                <MDBRow>
-                                    <MDBBtnGroup size="sm" >
-                                        <MDBBtn color="success" onClick={() => this.incrementMakes(shot)}>+M</MDBBtn>
-                                    </MDBBtnGroup>
-                                    <MDBBtnGroup size="sm" >
-                                        <MDBBtn color="primary" onClick={() => this.incrementAttemps(shot)}>+A</MDBBtn>
-                                    </MDBBtnGroup>
-                                    <MDBBtnGroup size="sm" >
-                                        <MDBBtn color="warning" onClick={() => this.decrementMakes(shot)}>-M</MDBBtn>
-                                    </MDBBtnGroup>
-                                    <MDBBtnGroup size="sm" >
-                                        <MDBBtn color="deep-orange" onClick={() => this.decrementAttemps(shot)}>-A</MDBBtn>
-                                    </MDBBtnGroup>
-                                    <MDBBtnGroup size="sm" >
-                                        <MDBBtn className="deep-purple accent-3" onClick={() => this.resetShot(shot)}><i class="fas fa-redo"></i></MDBBtn>
-                                    </MDBBtnGroup>
-                                </MDBRow>
-                            </MDBCol>
-                        </MDBRow>
-                    </MDBContainer>
-                </div>
+                <ShotListing shot={shot}
+                    incrementAttemps={this.incrementAttemps}
+                    decrementAttemps={this.decrementAttemps}
+                    incrementMakes={this.incrementMakes}
+                    decrementMakes={this.decrementMakes}
+                    deleteShot={this.deleteShot}
+                    resetShot={this.resetShot}
+                />
             )
         })
     }
--- client/src/components/shotlisting.component.jsx
+++ client/src/components/shotlisting.component.jsx
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBBtnGroup } from "mdbreact";
 
 class ShotListing extends Component {
     state = {}
+
+    getShotPercentClass = (percentage) => {
+
+        if (percentage >= 100) {
+            return 'royalblue';
+        }
+        else if (percentage >= 80) {
+            return '#4287f5';
+        }
+        else if (percentage >= 50) {
+            return 'green';
+        }
+        else if (percentage >= 33) {
+            return '#fc7b03';
+        }
+        else if (percentage < 80) {
+            return 'red';
+        }
+        else {
+            return 'black';
+        }
+    }
+
     render() {
+        const shot = this.props.shot;
+
         return (
-            <div></div>
+            <div className="shotlist-item" key={"x" + shot.x + ",y" + shot.y}>
+                <MDBContainer>
+                    <MDBRow size="12" style={{ width: "100%" }}>
+                        <MDBCol style={{ justifyContent: "center", alignItems: "center", margin: "auto" }}>
+                            <span className="court-mark" style={{ color: shot.markercolor }}>{shot.marker}</span>
+                        </MDBCol>
+                        <MDBCol size="9" lg="9">
+                            <MDBRow style={{ padding: "5px", alignItems: "center" }}>
+                                x: {shot.x}, y: {shot.y}
+                                <MDBBtnGroup size="sm" >
+                                    <MDBBtn color="danger" onClick={() => this.props.deleteShot(shot)}><i className="fas fa-trash-alt"></i></MDBBtn>
+                                </MDBBtnGroup>
+                            </MDBRow>
+                            <MDBRow style={{ padding: "5px" }}>
+                                FGM: {shot.makes} FGA: {shot.attemps} FG%: <span style={{ fontWeight: 500, color: this.getShotPercentClass(shot.makes / shot.attemps * 100) }}> {shot.attemps > 0 ? (shot.makes / shot.attemps * 100).toFixed(1) + "%" : "0.0%"}</span>
+                            </MDBRow>
+                            <MDBRow>
+                                <MDBBtnGroup size="sm" >
+                                    <MDBBtn color="success" onClick={() => this.props.incrementMakes(shot)}>+M</MDBBtn>
+                                </MDBBtnGroup>
+                                <MDBBtnGroup size="sm" >
+                                    <MDBBtn color="primary" onClick={() => this.props.incrementAttemps(shot)}>+A</MDBBtn>
+                                </MDBBtnGroup>
+                                <MDBBtnGroup size="sm" >
+                                    <MDBBtn color="warning" onClick={() => this.props.decrementMakes(shot)}>-M</MDBBtn>
+                                </MDBBtnGroup>
+                                <MDBBtnGroup size="sm" >
+                                    <MDBBtn color="deep-orange" onClick={() => this.props.decrementAttemps(shot)}>-A</MDBBtn>
+                                </MDBBtnGroup>
+                                <MDBBtnGroup size="sm" >
+                                    <MDBBtn className="deep-purple accent-3" onClick={() => this.props.resetShot(shot)}><i class="fas fa-redo"></i></MDBBtn>
+                                </MDBBtnGroup>
+                            </MDBRow>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </div>
         );
     }
 }
