210a212ed32d415eed8a01cc0fc1c085d3a09258
csimbulan <c.simbulan@hotmail.com>
2020-11-12 20:02:46 -0500

    require dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import 'mdbreact/dist/css/mdb.css';
 import { Auth0Provider } from '@auth0/auth0-react'
 import store from './store'
 import { Provider } from 'react-redux';
+require("dotenv").config();
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
